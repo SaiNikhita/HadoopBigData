5270557daf94322ebabddb93060e556b69eccf60
Sai Nikhita <sainikhitanayani@gmail.com>
2019-07-05 13:48:39 +0530

    Launching commands

---
+++ own1.sh
@@ -0,0 +1,8 @@
+sudo chown -R hduser076:hadoop /app/hadoop/tmp
+sudo chown -R hduser076:hadoop /usr/local/hadoop_store
+sudo chown -R hduser076:hadoop /usr/local/hadoop 
+sudo chmod -R 777 /app/hadoop/tmp
+sudo chmod -R 777 /usr/local/hadoop_store
+sudo chmod -R 777 /usr/local/hadoop
+
+
