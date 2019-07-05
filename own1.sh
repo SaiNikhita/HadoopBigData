1796906c0b6bcca265835816b108fbfe7f5f7493
Sai Nikhita <sainikhitanayani@gmail.com>
2019-07-05 13:49:34 +0530

    Launching Hadoop

--- own1.sh
+++ own1.sh
@@ -1,3 +1,5 @@
+bash
+
 sudo chown -R hduser076:hadoop /app/hadoop/tmp
 sudo chown -R hduser076:hadoop /usr/local/hadoop_store
 sudo chown -R hduser076:hadoop /usr/local/hadoop 
@@ -5,4 +7,5 @@ sudo chmod -R 777 /app/hadoop/tmp
 sudo chmod -R 777 /usr/local/hadoop_store
 sudo chmod -R 777 /usr/local/hadoop
 
-
+start-all.sh
+jps
