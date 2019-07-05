a3d0c08925e4a05a98107010f1b077b3f2c406cd
Sai Nikhita <sainikhitanayani@gmail.com>
2019-07-05 14:10:25 +0530

    Update hadoopCommands.sh

--- hadoopCommands.sh
+++ hadoopCommands.sh
@@ -3,3 +3,6 @@ hadoop fs -mkdir /Trial   #http://localhost:50070/explorer.html#/
 
 #copy file in local system to hadoop directory
 hadoop fs -put sample.txt /Trial
+
+#copy file from hadoop to local system
+hadoop fs -get /Trial/sample.txt /home/hduser076/
